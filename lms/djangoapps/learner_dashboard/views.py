5e7398eaa4baade7c05a9bc33d49ee2568466e39
Rene Sorel <rene@proversity.org>
2016-08-17 14:25:07 +0200

    never use edx pattern library

--- lms/djangoapps/learner_dashboard/views.py
+++ lms/djangoapps/learner_dashboard/views.py
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 """Learner dashboard views"""
+=======
+f>>"""Learner dashboard views"""
+import waffle
+>>>>>>> never use edx pattern library
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
 
@@ -42,7 +47,19 @@ def program_details(request, program_uuid):
         'show_dashboard_tabs': True,
         'nav_hidden': True,
         'disable_courseware_js': True,
+<<<<<<< HEAD
         'uses_pattern_library': True,
+<<<<<<< HEAD
+=======
+        'user_preferences': get_user_preferences(request.user),
+        'program_data': program_data,
+        'course_data': course_data,
+        'certificate_data': certificate_data
+=======
+        'uses_pattern_library': False,
+        'user_preferences': get_user_preferences(request.user)
+>>>>>>> never use edx pattern library
+>>>>>>> never use edx pattern library
     }
 
     return render_to_response('learner_dashboard/program_details.html', context)
