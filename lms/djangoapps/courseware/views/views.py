fa26529b2ded9dec2373b107c8549838cd817313
jagonzalr <antonio@proversity.org>
2016-10-19 15:45:14 +0200

    Show waiting for activation pre-enrollment

--- lms/djangoapps/courseware/views/views.py
+++ lms/djangoapps/courseware/views/views.py
@@ -739,6 +739,7 @@ def course_about(request, course_id):
             return redirect(reverse(course_home_url_name(course.id), args=[text_type(course.id)]))
 
         registered = registered_for_course(course, request.user)
+        user_is_active = request.user.is_active
 
         staff_access = bool(has_access(request.user, 'staff', course))
         studio_url = get_studio_url(course, 'settings/details')
@@ -824,6 +825,7 @@ def course_about(request, course_id):
             'staff_access': staff_access,
             'studio_url': studio_url,
             'registered': registered,
+            'user_is_active': user_is_active,
             'course_target': course_target,
             'is_cosmetic_price_enabled': settings.FEATURES.get('ENABLE_COSMETIC_DISPLAY_PRICE'),
             'course_price': course_price,
