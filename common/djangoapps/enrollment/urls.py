c5fc7142fb5e99bb70e2d8a1020b05f215d4b724
jagonzalr <antonio@proversity.org>
2016-11-04 11:50:53 +0200

    get student progress in a course

--- common/djangoapps/enrollment/urls.py
+++ common/djangoapps/enrollment/urls.py
@@ -5,7 +5,17 @@ URLs for the Enrollment API
 from django.conf import settings
 from django.conf.urls import url
 
+<<<<<<< HEAD
 from .views import EnrollmentCourseDetailView, EnrollmentListView, EnrollmentView
+=======
+from .views import (
+    EnrollmentView,
+    EnrollmentListView,
+    EnrollmentCourseDetailView,
+    EnrollmentCoursePassedView
+)
+
+>>>>>>> get student progress in a course
 
 urlpatterns = [
     url(r'^enrollment/{username},{course_key}$'.format(
@@ -15,6 +25,20 @@ urlpatterns = [
     url(r'^enrollment/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
         EnrollmentView.as_view(), name='courseenrollment'),
     url(r'^enrollment$', EnrollmentListView.as_view(), name='courseenrollments'),
+<<<<<<< HEAD
     url(r'^course/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
         EnrollmentCourseDetailView.as_view(), name='courseenrollmentdetails'),
 ]
+=======
+    url(
+        r'^course/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
+        EnrollmentCourseDetailView.as_view(),
+        name='courseenrollmentdetails'
+    ),
+    url(
+        r'^course/{course_key}/passed$'.format(course_key=settings.COURSE_ID_PATTERN),
+        EnrollmentCoursePassedView.as_view(),
+        name='courseenrollmentpassed'
+    ),
+)
+>>>>>>> get student progress in a course
