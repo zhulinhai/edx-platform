b75b8dbdf49438c08aef1d801dfed1615dbb603f
Maksim Sokolskiy <misokolsky@gmail.com>
2017-01-03 19:37:55 +0200

    fix(course_category): fix python regex bug for Django slug field - change `\w+` to `[\w-]+`

--- common/djangoapps/course_category/urls.py
+++ common/djangoapps/course_category/urls.py
@@ -6,6 +6,5 @@ import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.course_category_list, name='course_category_list'),
-    url(r'^(?P<slug>\w+)$', views.course_category, name='course_category'),
+    url(r'^(?P<slug>[\w-]+)$', views.course_category, name='course_category'),
 )
-
