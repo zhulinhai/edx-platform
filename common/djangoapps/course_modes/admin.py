f7b4ee61e1abe3797d7a4028861e1733d355731a
lidijarad <radovanovic.lidija22@gmail.com>
2017-01-18 08:42:27 +0200

    order course modes courses by display name in admin

--- common/djangoapps/course_modes/admin.py
+++ common/djangoapps/course_modes/admin.py
@@ -39,9 +39,10 @@ class CourseModeForm(forms.ModelForm):
     class Meta(object):
         model = CourseMode
         fields = '__all__'
+        
 
     mode_slug = forms.ChoiceField(choices=COURSE_MODE_SLUG_CHOICES, label=_("Mode"))
-    course_id = forms.ChoiceField(choices=[(course.id, course.display_name) for course in CourseOverview.objects.all()], label=_("Course"))
+    course_id = forms.ChoiceField(choices=[(course.id, course.display_name) for course in CourseOverview.objects.all().order_by('display_name')], label=("Course"))
 
     # The verification deadline is stored outside the course mode in the verify_student app.
     # (we used to use the course mode expiration_datetime as both an upgrade and verification deadline).
