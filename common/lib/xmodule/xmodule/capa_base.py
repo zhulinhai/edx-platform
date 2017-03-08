0544ce4e7923a8753906f37a1ec39aff85346cb3
Jose Antonio Gonzalez <antonio@proversity.org>
2017-03-08 14:01:20 +0200

    add field to show or not number of attemprs

--- common/lib/xmodule/xmodule/capa_base.py
+++ common/lib/xmodule/xmodule/capa_base.py
@@ -225,6 +225,12 @@ class CapaFields(object):
                "or to report an issue, please contact moocsupport@mathworks.com"),
         scope=Scope.settings
     )
+    show_attempts = Boolean(
+        display_name="Show attempts",
+        help="This will show or not the attempts for the problem.",
+        scope=Scope.settings,
+        default=True
+    )
 
 
 class CapaMixin(ScorableXBlockMixin, CapaFields):
@@ -740,6 +746,7 @@ class CapaMixin(ScorableXBlockMixin, CapaFields):
             'answer_notification_message': answer_notification_message,
             'has_saved_answers': self.has_saved_answers,
             'save_message': save_message,
+            'show_attempts': self.show_attempts
         }
 
         html = self.runtime.render_template('problem.html', context)
