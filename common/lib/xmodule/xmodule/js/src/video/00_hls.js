473b838f9aecee6de29ebc1910ecb53109da0bd8
Rene Sorel <rene@proversity.org>
2017-03-09 11:10:40 +0200

    fixed hls.js require define

--- common/lib/xmodule/xmodule/js/src/video/00_hls.js
+++ common/lib/xmodule/xmodule/js/src/video/00_hls.js
@@ -1,4 +1,3 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define('video/00_hls.js',[],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Hls = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 // Copyright Joyent, Inc. and other Node contributors.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
@@ -22,6 +21,36 @@
 //
 // Learn more at https://github.com/dailymotion/hls.js
 //
+(function(f) {
+  define = typeof define == "function" ? define : RequireJS.define;
+  define('video/00_hls.js', [], f);
+})(function() {
+        var define, module, exports;
+        return (function e(t, n, r) {
+                function s(o, u) {
+                    if (!n[o]) {
+                        if (!t[o]) {
+                            var a = typeof require == "function" && require;
+                            if (!u && a) return a(o, !0);
+                            if (i) return i(o, !0);
+                            var f = new Error("Cannot find module '" + o + "'");
+                            throw f.code = "MODULE_NOT_FOUND", f
+                        }
+                        var l = n[o] = {
+                            exports: {}
+                        };
+                        t[o][0].call(l.exports, function(e) {
+                            var n = t[o][1][e];
+                            return s(n ? n : e)
+                        }, l, l.exports, e, t, n, r)
+                    }
+                    return n[o].exports
+                }
+                var i = typeof require == "function" && require;
+                for (var o = 0; o < r.length; o++) s(r[o]);
+                return s
+            })({
+                    1: [function(_dereq_, module, exports) {
 function EventEmitter() {
   this._events = this._events || {};
   this._maxListeners = this._maxListeners || undefined;
@@ -15416,5 +15445,3 @@ exports.default = XhrLoader;
 
 },{"50":50}]},{},[38])(38)
 });
-
-//# sourceMappingURL=src/video/00_hls.js.map
