a180efd9a31621ab1b2ee7507cf9f09049d09117
Giulio Gratta <giuliog@stanford.edu>
2017-03-24 15:28:13 -0700

    Fix instructor analytics tests

--- lms/djangoapps/instructor_analytics/tests/test_basic.py
+++ lms/djangoapps/instructor_analytics/tests/test_basic.py
@@ -21,7 +21,6 @@ from instructor_analytics.basic import (
 )
 from instructor_analytics.basic import student_responses
 from opaque_keys.edx.keys import CourseKey
-from opaque_keys.edx.locations import Location
 from opaque_keys.edx.locator import UsageKey
 from openedx.core.djangoapps.course_groups.tests.helpers import CohortFactory
 from student.models import CourseEnrollment, CourseEnrollmentAllowed
@@ -602,30 +601,7 @@ class TestStudentResponsesAnalyticsBasic(ModuleStoreTestCase):
         self.student = UserFactory()
         CourseEnrollment.enroll(self.student, self.course.id)
 
-    def test_empty_course(self):
-        self.create_student()
-        datarows = list(student_responses(self.course))
-        self.assertEqual(datarows, [])
-
-    def test_full_course_no_students(self):
-        datarows = list(student_responses(self.course))
-        self.assertEqual(datarows, [])
-
-    def test_invalid_module_state(self):
-        self.problem_location = Location("edX", "graded", "2012_Fall", "problem", "H1P2")
-        self.create_student()
-        StudentModuleFactory.create(
-            course_id=self.course.id,
-            module_state_key=self.problem_location,
-            student=self.student,
-            grade=0,
-            state=u'{"student_answers":{"fake-problem":"No idea"}}}'
-        )
-        datarows = list(student_responses(self.course))
-        #Invalid module state response will be skipped, so datarows should be empty
-        self.assertEqual(len(datarows), 0)
-
-    def test_problem_with_student_answer_and_answers(self):
+    def create_course_structure(self):
         section = ItemFactory.create(
             parent_location=self.course.location,
             category='chapter',
@@ -647,6 +623,34 @@ class TestStudentResponsesAnalyticsBasic(ModuleStoreTestCase):
             category='problem',
             display_name=u'test problem',
         )
+        return section, sub_section, unit, problem
+
+    def test_empty_course(self):
+        self.create_student()
+        datarows = list(student_responses(self.course))
+        self.assertEqual(datarows, [])
+
+    def test_full_course_no_students(self):
+        datarows = list(student_responses(self.course))
+        self.assertEqual(datarows, [])
+
+    def test_invalid_module_state(self):
+        section, sub_section, unit, problem = self.create_course_structure()
+        self.create_student()
+        StudentModuleFactory.create(
+            course_id=self.course.id,
+            module_state_key=problem.location,
+            student=self.student,
+            grade=0,
+            state=u'{"student_answers":{"fake-problem":"No idea"}}}'
+        )
+        course_with_children = modulestore().get_course(self.course.id, depth=4)
+        datarows = list(student_responses(course_with_children))
+        #Invalid module state response will be skipped, so datarows should be empty
+        self.assertEqual(len(datarows), 0)
+
+    def test_problem_with_student_answer_and_answers(self):
+        section, sub_section, unit, problem = self.create_course_structure()
         submit_and_compare_valid_state = ItemFactory.create(
             parent_location=unit.location,
             category='submit-and-compare',
@@ -703,14 +707,15 @@ class TestStudentResponsesAnalyticsBasic(ModuleStoreTestCase):
         self.assertEqual(datarows[3][-1], u'problem_id=content library response1')
 
     def test_problem_with_no_answer(self):
-        problem_location = Location('edX', 'graded', '2012_Fall', 'problem', 'H2P1')
+        section, sub_section, unit, problem = self.create_course_structure()
         self.create_student()
         StudentModuleFactory.create(
             course_id=self.course.id,
-            module_state_key=problem_location,
+            module_state_key=problem.location,
             student=self.student,
             grade=0,
             state=u'{"answer": {"problem_id": "123"}}',
         )
-        datarows = list(student_responses(self.course))
+        course_with_children = modulestore().get_course(self.course.id, depth=4)
+        datarows = list(student_responses(course_with_children))
         self.assertEqual(datarows[0][-1], None)
