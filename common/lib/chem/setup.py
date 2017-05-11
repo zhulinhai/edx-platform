7d374437bb3e3831616674e6ec13c4906572933a
nicovanniekerk <nicovanniekerk>
2017-05-11 14:11:17 +0200

    updating nltk

--- common/lib/chem/setup.py
+++ common/lib/chem/setup.py
@@ -8,6 +8,10 @@ setup(
         "pyparsing==2.0.7",
         "numpy==1.6.2",
         "scipy==0.14.0",
+<<<<<<< HEAD
         "nltk==3.2.5",
+=======
+        "nltk==3.2.2",
+>>>>>>> updating nltk
     ],
 )
