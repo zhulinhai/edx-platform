db1fc694a7d36e17992793c88603fae0ecc6ab4d
stv <stv@stanford.edu>
2017-05-23 14:41:24 +0200

    Unfork course rerun tests
    While edX enables course reruns by default, we disable it on all of our
    instances. Instead of forking the tests to accomodate, we should instead
    enable the feature during tests.  The added benefit here is that this
    ensures that we continue testing the feature, even though we aren't
    (currently) using it.

--- openedx/stanford/cms/envs/test.py
+++ openedx/stanford/cms/envs/test.py
@@ -1,6 +1,7 @@
 from cms.envs.test import *
 
 
+FEATURES['ALLOW_COURSE_RERUNS'] = True
 # Remove sneakpeek during tests to prevent unwanted redirect
 MIDDLEWARE_CLASSES = tuple([
     mwc for mwc in MIDDLEWARE_CLASSES
