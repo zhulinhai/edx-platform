3c767d2c25a5df3cc0ee389c70ee74db01d392ea
Nico van Niekerk <nico.niekerk@gmail.com>
2017-05-30 13:46:25 +0200

    UPD: adding submissions endoint url

--- lms/urls.py
+++ lms/urls.py
@@ -64,6 +64,9 @@ urlpatterns = [
     url(r'^dashboard/?$', student_views.student_dashboard, name='dashboard'),
     url(r'^change_enrollment$', student_views.change_enrollment, name='change_enrollment'),
 
+	# Submissions api
+    url(r'^api/submissions/', include('submissions.urls')),
+	
     # Event tracking endpoints
     url(r'', include('track.urls')),
 
