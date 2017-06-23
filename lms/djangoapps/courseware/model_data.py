adfbcf490b5ab169bce11ce8ebf633af5be1f3ae
lidijarad <radovanovic.lidija22@gmail.com>
2017-06-23 12:42:23 +0200

    remove assert logic from FieldDataCache get and has methods

--- lms/djangoapps/courseware/model_data.py
+++ lms/djangoapps/courseware/model_data.py
@@ -815,11 +815,11 @@ class FieldDataCache(object):
         Raises: KeyError if key isn't found in the cache
         """
 
-        if key.scope.user == UserScope.ONE and not self.user.is_anonymous():
+        #if key.scope.user == UserScope.ONE and not self.user.is_anonymous():
             # If we're getting user data, we expect that the key matches the
             # user we were constructed for.
             # assert key.user_id == self.user.id
-            pass
+        #    pass
 
         if key.scope not in self.cache:
             raise KeyError(key.field_name)
@@ -897,11 +897,11 @@ class FieldDataCache(object):
         Returns: bool
         """
 
-        if key.scope.user == UserScope.ONE and not self.user.is_anonymous():
+        #if key.scope.user == UserScope.ONE and not self.user.is_anonymous():
             # If we're getting user data, we expect that the key matches the
             # user we were constructed for.
             # assert key.user_id == self.user.id
-            pass
+        #    pass
         if key.scope not in self.cache:
             return False
 
