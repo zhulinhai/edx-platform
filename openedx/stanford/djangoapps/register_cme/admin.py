f5984fbd4187f4af65fb39b070f91870203d869b
Po Tsui <potsui@stanford.edu>
2017-07-06 14:03:50 -0700

    Change `ExtraInfo` to user fields, add search
    `Register_cme/extrainfo` in Django Admin was previously displaying users
    as `ExtraInfo` objects which admins had to click on individually to see
    each user's information. Each user is now displayed with fields:
    username, email, last and first name. Username is clickable to view more
    information. Added search bar enables search for users matching query
    for username, email, last and first name.

--- openedx/stanford/djangoapps/register_cme/admin.py
+++ openedx/stanford/djangoapps/register_cme/admin.py
@@ -5,4 +5,16 @@ from django.contrib import admin
 
 from .models import ExtraInfo
 
-admin.site.register(ExtraInfo)
+class ExtraInfoAdmin(admin.ModelAdmin):
+    """ Admin interface for ExtraInfo model. """
+    list_display = ('user', 'get_email', 'last_name', 'first_name',)
+    search_fields = ('user__username', 'user__email', 'last_name', 'first_name',)
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email address'
+
+    class Meta(object):
+        model = ExtraInfo
+
+admin.site.register(ExtraInfo, ExtraInfoAdmin)
