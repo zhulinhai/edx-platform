7fc4048c010ab0623ee238465e7dd2fbfe5d33c8
Giulio Gratta <giuliog@stanford.edu>
2017-07-11 11:18:37 -0700

    Don't fork Submit Button tests

--- common/lib/xmodule/xmodule/tests/test_capa_module.py
+++ common/lib/xmodule/xmodule/tests/test_capa_module.py
@@ -993,30 +993,6 @@ class CapaModuleTest(unittest.TestCase):
         self.assertTrue('success' in result and result['success'])
 
     def test_submit_button_name(self):
-
-        # If last attempt, button name changes to "Submit"
-        # Just in case, we also check what happens if we have
-        # more attempts than allowed.
-        attempts = random.randint(1, 10)
-        module = CapaFactory.create(attempts=attempts - 1, max_attempts=attempts)
-        self.assertEqual(module.submit_button_name(), "Final Submit")
-
-        module = CapaFactory.create(attempts=attempts, max_attempts=attempts)
-        self.assertEqual(module.submit_button_name(), "Final Submit")
-
-        module = CapaFactory.create(attempts=attempts + 1, max_attempts=attempts)
-        self.assertEqual(module.submit_button_name(), "Final Submit")
-
-        module = CapaFactory.create(attempts=attempts - 2, max_attempts=attempts)
-        self.assertEqual(module.submit_button_name(), "Submit")
-
-        module = CapaFactory.create(attempts=attempts - 3, max_attempts=attempts)
-        self.assertEqual(module.submit_button_name(), "Submit")
-
-        # If no limit on attempts
-        module = CapaFactory.create(attempts=attempts - 3)
-        self.assertEqual(module.submit_button_name(), "Submit")
-
         module = CapaFactory.create(attempts=0)
         self.assertEqual(module.submit_button_name(), "Submit")
 
