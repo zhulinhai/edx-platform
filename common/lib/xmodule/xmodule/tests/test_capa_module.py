46884623e3d42a5c8068a4e40d4163701d8103ba
Giulio Gratta <giuliog@stanford.edu>
2017-07-11 11:18:51 -0700

    Convert timed problem test to use current function

--- common/lib/xmodule/xmodule/tests/test_capa_module.py
+++ common/lib/xmodule/xmodule/tests/test_capa_module.py
@@ -1634,7 +1634,7 @@ class CapaModuleTest(unittest.TestCase):
         rendered = module.start_problem()
 
         self.assertIsNotNone(module.time_started)
-        self.assertTrue(module.should_show_submit_button())
+        self.assertTrue(module.should_enable_submit_button())
         self.assertFalse(module.closed())
 
     def test_time_expired(self):
@@ -1672,7 +1672,7 @@ class CapaModuleTest(unittest.TestCase):
                 get_request_dict = {CapaFactory.answer_key(): '3'}
                 results = module.submit_problem(get_request_dict)
                 self.assertFalse(module.should_show_reset_button())
-                self.assertFalse(module.should_show_submit_button())
+                self.assertFalse(module.should_enable_submit_button())
 
     def test_timed_save(self):
         """
