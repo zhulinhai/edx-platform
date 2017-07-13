34e33a34814f3d866ef09b4ac09f06895ce67d99
Jose Antonio Gonzalez <antonio@proversity.org>
2017-07-13 10:03:31 +0200

    change email var

--- lms/djangoapps/instructor/views/api.py
+++ lms/djangoapps/instructor/views/api.py
@@ -2555,6 +2555,7 @@ def list_forum_members(request, course_id):
 
 def send_email_to_specific_learners(course, learners, template_name, from_addr, subject, message):
     log.info(u'send_email_to_specific_learners')
+    log.info(learners)
     connection = get_connection()
     connection.open()
 
@@ -2604,7 +2605,7 @@ def send_email_to_specific_learners(course, learners, template_name, from_addr, 
                 subject,
                 plaintext_msg,
                 from_addr,
-                [email],
+                [learner],
                 connection=connection
             )
             email_msg.attach_alternative(html_msg, 'text/html')
@@ -2612,7 +2613,7 @@ def send_email_to_specific_learners(course, learners, template_name, from_addr, 
             # Send email
             connection.send_messages([email_msg])
         except Exception as e:
-            print e
+            log.info(e)
             pass
 
 
@@ -2644,6 +2645,8 @@ def send_email(request, course_id):
     subject = request.POST.get("subject")
     message = request.POST.get("message")
     specific_learners = _split_input_list(request.POST.get("specific_learners"))
+    log.info(u'specific_learners')
+    log.info(specific_learners)
 
     # allow two branding points to come from Site Configuration: which CourseEmailTemplate should be used
     # and what the 'from' field in the email should be
@@ -2692,7 +2695,8 @@ def send_email(request, course_id):
         send_email_to_specific_learners(course, specific_learners, template_name, from_addr, subject, message)
         response_payload = {
             'course_id': course_id.to_deprecated_string(),
-            'success': True
+            'success': True,
+            'from_addr': from_addr
         }
         return JsonResponse(response_payload)
     else:
