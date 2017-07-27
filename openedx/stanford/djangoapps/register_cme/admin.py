507be5c8923b05304b223785cdba79ae7513f48a
caseylitton <caseylitton@gmail.com>
2017-07-27 12:19:18 -0700

    Revert "Change `ExtraInfo` to user fields, add search"
    This reverts commit f5984fbd4187f4af65fb39b070f91870203d869b.

--- openedx/stanford/djangoapps/register_cme/admin.py
+++ openedx/stanford/djangoapps/register_cme/admin.py
@@ -5,16 +5,4 @@ from django.contrib import admin
 
 from .models import ExtraInfo
 
-class ExtraInfoAdmin(admin.ModelAdmin):
-    """ Admin interface for ExtraInfo model. """
-    list_display = ('user', 'get_email', 'last_name', 'first_name',)
-    search_fields = ('user__username', 'user__email', 'last_name', 'first_name',)
-
-    def get_email(self, obj):
-        return obj.user.email
-    get_email.short_description = 'Email address'
-
-    class Meta(object):
-        model = ExtraInfo
-
-admin.site.register(ExtraInfo, ExtraInfoAdmin)
+admin.site.register(ExtraInfo)
