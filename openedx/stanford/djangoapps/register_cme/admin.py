d2adf86767857e9b57527c3db1d720b1f8f086a2
Po Tsui <potsui@stanford.edu>
2017-08-14 17:46:26 -0700

    Make `user` field read-only in `ExtraInfo` detail
    Previously, server would hang trying to load an `ExtraInfo` detail page,
    because the `user` field was rendering as a dropdown select menu loading
    all users in the system. We fix this by making the field read-only.

--- openedx/stanford/djangoapps/register_cme/admin.py
+++ openedx/stanford/djangoapps/register_cme/admin.py
@@ -5,4 +5,17 @@ from django.contrib import admin
 
 from .models import ExtraInfo
 
-admin.site.register(ExtraInfo)
+
+class ExtraInfoAdmin(admin.ModelAdmin):
+    """
+    Admin interface for ExtraInfo model.
+    """
+
+    readonly_fields = (
+        'user',
+    )
+
+    class Meta(object):
+        model = ExtraInfo
+
+admin.site.register(ExtraInfo, ExtraInfoAdmin)
