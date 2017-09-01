512557bf1f2693db90518b93d21a4e779ec5ece7
Jhony Avella <luis.moreno@edunext.co>
2017-09-01 10:08:35 -0500

    added raven configuration

--- lms/djangoapps/courseware/access.py
+++ lms/djangoapps/courseware/access.py
@@ -18,6 +18,8 @@ from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.utils.timezone import UTC
 
+from raven.contrib.django.raven_compat.models import client
+
 from opaque_keys.edx.keys import CourseKey, UsageKey
 
 from xblock.core import XBlock
@@ -861,26 +863,25 @@ def user_has_membership(user):
 
 
     from subscriptions import appmysqldb
-    mysql_host = getattr(settings, "SUBSCRIPTION_MYSQL_HOST", "localhost")
+    mysql_host = getattr(settings, "SUBSCRIPTION_MYSQL_HOST", "172.31.23.49")
     mysql_database = getattr(settings, "SUBSCRIPTION_MYSQL_DB_NAME", "edxapp")
     mysql_user = getattr(settings, "SUBSCRIPTION_MYSQL_USER", "edxapp001")
     mysql_pwd = getattr(settings, "SUBSCRIPTION_MYSQL_PASSWORD", "password")
 
     db = appmysqldb.mysql(mysql_host, 3306, mysql_database, mysql_user, mysql_pwd)
-    q = "SELECT id,email FROM auth_user WHERE username='%s' LIMIT 1" % (user)
+    q = "SELECT id FROM auth_user WHERE username='%s' LIMIT 1" % (user)
     db.query(q)
     res = db.fetchall()
     for row in res:
         user_id = row[0]
-        edx_email = row[1]
+        client.captureMessage(user_id)
 
-    q = "SELECT aux_subscription_id,type_sus FROM aux_subscriptions WHERE user_id='%s' LIMIT 1" % (user_id)
+    q = "SELECT type_sus FROM aux_subscriptions WHERE user_id='%s' LIMIT 1" % (user_id)
     db.query(q)
     res = db.fetchall()
     for row in res:
-        check_sus_id = row[0]
-        get_type = row[1]
-
+        get_type = row[0]
+        client.captureMessage(get_type)
 
     if get_type != 1 and get_type != 4:
         return ACCESS_GRANTED
