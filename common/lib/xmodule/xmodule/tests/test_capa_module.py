32ab592f452e099e405c6638a5f813a0ed44b452
Awais Jibran <awaisdar001@gmail.com>
2017-10-24 15:32:45 +0500

    Fix Show Answer button display for the value "Answered" in studio
    In docs it is explained that we should show "Show Answer" button for "Answered" value when the user has correctly submitted the answer. The current behavior is such, even if the learner has answered the question wrong, the "Show Answer" button would be available.
    
    EDUCATOR-1563

--- common/lib/xmodule/xmodule/tests/test_capa_module.py
+++ common/lib/xmodule/xmodule/tests/test_capa_module.py
@@ -33,7 +33,7 @@ from xblock.fields import ScopeIds
 from . import get_test_system
 from pytz import UTC
 from capa.correctmap import CorrectMap
-from ..capa_base_constants import RANDOMIZATION
+from ..capa_base_constants import RANDOMIZATION, SHOWANSWER
 
 
 class CapaFactory(object):
@@ -437,6 +437,27 @@ class CapaModuleTest(unittest.TestCase):
                                             due=self.yesterday_str,
                                             graceperiod=self.two_day_delta_str)
         self.assertTrue(still_in_grace.answer_available())
+
+    def test_showanswer_answered(self):
+        answer_wrong = CapaFactory.create(
+            showanswer=SHOWANSWER.ANSWERED,
+            max_attempts="1",
+            attempts="0",
+            due=self.tomorrow_str,
+            correct=False
+        )
+        self.assertFalse(answer_wrong.answer_available())
+
+
+        answer_correct = CapaFactory.create(
+            showanswer=SHOWANSWER.ANSWERED,
+            max_attempts="1",
+            attempts="0",
+            due=self.tomorrow_str,
+            correct=True
+        )
+        self.assertTrue(answer_correct.answer_available())
+
 
     @ddt.data('', 'other-value')
     def test_show_correctness_other(self, show_correctness):
