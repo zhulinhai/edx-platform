f4c5d5e9fa10b642010f960a10d5be44ae563be8
Nico van Niekerk <nico.niekerk@gmail.com>
2017-10-30 09:33:39 +0200

    TRY: new build hack try one

---
+++ build_and_deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+IP="$(curl -s icanhazip.com)"
+
+echo "Received IP = $IP, deplying to $1"
+
+if [[ "$1" == "development" ]]; then
+	echo "Adding IP to EC2"
+	aws ec2 authorize-security-group-ingress --group-id $STAGING_SEC_GROUP --protocol tcp --port 22 --cidr "$IP/32"
+	sleep 2
+	echo "Running play to update Microsites and Assets"
+  
+  ssh ubuntu@34.240.235.247 "sudo apt-get -y install  git"
+  ssh ubuntu@34.240.235.247 "sudo rm -r /edx/app/edxapp/circleci_builds && sudo mkdir -p /edx/app/edxapp && cd /edx/app/edxapp && sudo git clone https://github.com/proversity-org/circleci_builds.git"
+  ssh ubuntu@34.240.235.247 "cd /edx/app/edxapp/circleci_builds && sudo /bin/bash run_build.sh development"
+
+	ERROR=$?
+	sleep 2
+	echo "Removing IP from EC2"
+	aws ec2 revoke-security-group-ingress --group-id $STAGING_SEC_GROUP --protocol tcp --port 22 --cidr "$IP/32"
+fi
+
+
+if [ "$ERROR" -eq 0 ];
+then
+    echo "Updated!"
+	exit 0
+else
+    echo "Update failed!"
+	exit 1
+fi
