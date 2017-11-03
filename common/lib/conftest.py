b3f81b6d91e38c30054daa9ce3a1d520ef2af98e
Nico van Niekerk <nico.niekerk@gmail.com>
2017-11-03 14:21:00 +0200

    try sys convert

--- common/lib/conftest.py
+++ common/lib/conftest.py
@@ -1,5 +1,19 @@
+<<<<<<< HEAD
 """Code run by pylint before running any tests."""
 
 # Patch the xml libs before anything else.
 from safe_lxml import defuse_xml_libs
 defuse_xml_libs()
+=======
+from django.conf import settings
+import sys
+
+
+def pytest_configure():
+    """
+    Use Django's default settings for tests in common/lib.
+    """
+    reload(sys)  
+    sys.setdefaultencoding('Cp1252')
+    settings.configure()
+>>>>>>> try sys convert
--- common/lib/xmodule/xmodule/modulestore/tests/test_mongo.py
+++ common/lib/xmodule/xmodule/modulestore/tests/test_mongo.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 """
 Unit tests for the Mongo modulestore
 """
