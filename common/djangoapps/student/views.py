90976540da23d78419af9f3c7db771c619fb0ff7
renevatium <rene@proversity.org>
2017-12-11 16:57:20 +0200

    fix bad variable assignment in password reset confirm

--- common/djangoapps/student/views.py
+++ common/djangoapps/student/views.py
@@ -28,7 +28,7 @@ from django.core.validators import ValidationError, validate_email
 from django.db import IntegrityError, transaction
 from django.db.models.signals import post_save
 from django.dispatch import Signal, receiver
-from django.http import Http404, HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.http import Http404, HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseRedirect
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.utils.encoding import force_bytes, force_text
@@ -2904,8 +2904,10 @@ def password_reset_confirm_wrapper(request, uidb36=None, token=None):
             user.is_active = True
             user.save()
 
-    return render_to_response('registration/password_reset_confirm.html',
-        response.context_data if hasattr(response, 'context_data') else None)
+    if not isinstance(response, HttpResponseRedirect):
+        return render_to_response('registration/password_reset_confirm.html', response.context_data)
+    else:
+        return response
 
 
 def reactivation_email_for_user(user):
