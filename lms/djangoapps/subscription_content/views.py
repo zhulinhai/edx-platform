7dd1bcad07177e9c1bc56f639bda49663d022aa6
Nico van Niekerk <nico.niekerk@gmail.com>
2018-01-04 12:43:39 +0200

    FIX: changing tabs for spaces

--- lms/djangoapps/subscription_content/views.py
+++ lms/djangoapps/subscription_content/views.py
@@ -19,45 +19,45 @@ def dashboard(request):
   context = {}
   client_id =  configuration_helpers.get_value('BIBBLIO_CLIENT_ID', settings.BIBBLIO_CLIENT_ID)
   client_secret =  configuration_helpers.get_value('BIBBLIO_CLIENT_SECRET', settings.BIBBLIO_CLIENT_SECRET)
-	token = get_access_token(client_id, client_secret)
-	if 'access_token' in token:
-		catalog_ids = get_subscription_catalog_ids(request.user)
-		if catalog_ids:
-			context = { 'message': '', 'content_items': {} }
-			content_items = get_content_items(token['access_token'], catalog_ids)
-			if 'results'in content_items:
-				for item in content_items['results']:
-					context['content_items'][item['contentItemId']] = item
-					content_item = get_content_item(token['access_token'], item['contentItemId'])
-					if 'contentItemId' in content_item:
-						if not content_item['thumbnail'] and content_item['moduleImage']:
-							content_item['thumbnail'] = content_item['moduleImage']
-						context['content_items'][item['contentItemId']]['meta'] = content_item
-					else:
-						context['message'] = context['message']+"<br />"+item['name']+": "+content_item['message']
-			else:
-				context['message'] = context['message']+"<br />"+content_items['message']
-		else:
-			context = { 'message': token['message'] }
+  token = get_access_token(client_id, client_secret)
+  if 'access_token' in token:
+    catalog_ids = get_subscription_catalog_ids(request.user)
+    if catalog_ids:
+      context = { 'message': '', 'content_items': {} }
+      content_items = get_content_items(token['access_token'], catalog_ids)
+      if 'results'in content_items:
+        for item in content_items['results']:
+          context['content_items'][item['contentItemId']] = item
+          content_item = get_content_item(token['access_token'], item['contentItemId'])
+          if 'contentItemId' in content_item:
+            if not content_item['thumbnail'] and content_item['moduleImage']:
+              content_item['thumbnail'] = content_item['moduleImage']
+            context['content_items'][item['contentItemId']]['meta'] = content_item
+          else:
+            context['message'] = context['message']+"<br />"+item['name']+": "+content_item['message']
+      else:
+        context['message'] = context['message']+"<br />"+content_items['message']
+    else:
+      context = { 'message': token['message'] }
 
-	response = render_to_response('subscription_content/dashboard.html', context)
-	set_user_info_cookie(response, request)
-	return response
+  response = render_to_response('subscription_content/dashboard.html', context)
+  set_user_info_cookie(response, request)
+  return response
 
 
 def catalogues(request):
-	token = get_access_token(client_id, client_secret)
-	if 'access_token' in token:
-		context = { 'message': '', 'catalogues': {} }
-		catalogues = get_catalogues(token['access_token'])
-		if 'results' in catalogues:
-			for catalogue in catalogues['results']:
-				context['catalogues'][catalogue['catalogueId']] = catalogue
-		else:
-			context['message'] = context['message']+"<br />"+catalogues['message']
-	else:
-		context = { 'message': token['message'] }
+  token = get_access_token(client_id, client_secret)
+  if 'access_token' in token:
+    context = { 'message': '', 'catalogues': {} }
+    catalogues = get_catalogues(token['access_token'])
+    if 'results' in catalogues:
+      for catalogue in catalogues['results']:
+        context['catalogues'][catalogue['catalogueId']] = catalogue
+    else:
+      context['message'] = context['message']+"<br />"+catalogues['message']
+  else:
+    context = { 'message': token['message'] }
 
-	response = render_to_response('subscription_content/all.html', context)
-	set_user_info_cookie(response, request)
-	return response
+  response = render_to_response('subscription_content/all.html', context)
+  set_user_info_cookie(response, request)
+  return response
