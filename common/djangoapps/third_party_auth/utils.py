15844162680c25b68db3d6105343b8e5060c58e1
Diego Rojas <diego.rojas@edunext.co>
2018-02-15 14:37:18 -0500

    Improve logic in while

--- common/djangoapps/third_party_auth/utils.py
+++ common/djangoapps/third_party_auth/utils.py
@@ -4,10 +4,12 @@ from django.contrib.auth.models import User
 
 
 def generate_username(username):
-	validate_user = User.objects.filter(username=username).exists()
-	while validate_user:
-		random = ''.join(["%s" % randint(0, 9) for num in range(0, 4)])
-		new_username = username + '_{}'.format(random)
-		return new_username
+    new_username = username
+    user_exists = User.objects.filter(username=new_username).exists()
 
-	return username
+    while user_exists:
+        random = ''.join(["%s" % randint(0, 9) for num in range(0, 4)])
+        new_username = username + '_{}'.format(random)
+        user_exists = User.objects.filter(username=new_username).exists()
+
+    return new_username
