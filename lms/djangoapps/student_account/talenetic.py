e345c5bdb2834a7dbb355073c661ba5a3706da7b
nicovanniekerk <nico@proversity.org>
2018-04-04 11:15:49 +0200

    ENH: SSO bullied into working

--- lms/djangoapps/student_account/talenetic.py
+++ lms/djangoapps/student_account/talenetic.py
@@ -1,3 +1,4 @@
+from six.moves.urllib_parse import urlencode, unquote
 import jwt
 import json
 from django.conf import settings
@@ -18,79 +19,106 @@ class TaleneticOAuth2(BaseOAuth2):
     settings_dict = settings.CUSTOM_BACKENDS.get('talenetic')
     name = 'talenetic-oauth2'
     REDIRECT_STATE = False
-    ID_KEY = 'username'
-    STATE_PARAMETER = True
+    ID_KEY = 'emailaddress'
+    STATE_PARAMETER = False
     AUTHORIZATION_URL = settings_dict.get('AUTH_URL') # https://staging-alj.talenetic.com/jobseekers?clientId={clientid}&secretkey={secretkey}&urlredirect={redirecturl}
     ACCESS_TOKEN_URL = settings_dict.get('ACCESS_TOKEN_URL') # https://staging-alj.talenetic.com/api/sso/Getjwttoken?uid=
-    ACCESS_TOKEN_METHOD = 'POST'
+    ACCESS_TOKEN_METHOD = 'GET'
     REFRESH_TOKEN_URL = settings_dict.get('REFRESH_TOKEN_URL') # https://staging-alj.talenetic.com/api/sso/RefreshjwtToken?uid=NzIzMzUyOWYtZDkyYi00ZGUwLThhODMtNjBiOTk0NzZlMjVj
     REFRESH_TOKEN_METHOD = 'POST'
     RESPONSE_TYPE = 'code jwt_token'
-    REDIRECT_IS_HTTPS = True
+    REDIRECT_IS_HTTPS = False
     REVOKE_TOKEN_URL = settings_dict.get('LOGOUT_URL') # 'https://staging-alj.talenetic.com/api/logout'
     REVOKE_TOKEN_METHOD = 'GET'
 
-    # The order of the default scope is important
-    DEFAULT_SCOPE = ['openid', 'profile']
+    def get_scope_argument(self):
+        return {}
 
 
-    # def extra_data(self, user, uid, response, details=None, *args, **kwargs):
-    #     """
-    #     Return access_token and extra defined names to store in extra_data field
-    #     """
-    #     data = super(HarambeeOAuth2, self).extra_data(user, uid, response, details, *args, **kwargs)
-    #     data['id_token'] = kwargs['request_id_token']
-    #     return data
+    def auth_complete(self, *args, **kwargs):
+        """Completes login process, must return user instance"""
+        self.process_error(self.data)
+        state = self.validate_state()
+        access_url = "{}?uid={}".format(self.access_token_url(), self._get_uid())
+        response = self.request_access_token(
+            access_url,
+            data=self._get_creds(),
+            headers=self._get_creds(),
+            auth=self.auth_complete_credentials(),
+            method=self.ACCESS_TOKEN_METHOD
+        )
+        self.process_error(response)
+        return self.do_auth(response['jwt_token'], response=response,
+                            *args, **kwargs)
+
+    def do_auth(self, jwt_token, *args, **kwargs):
+        data = self.user_data(jwt_token, *args, **kwargs)
+        response =  kwargs.get('response') or {}
+        response.update(data or {})
+        if 'access_token' not in response:
+            response['access_token'] = jwt_token
+        kwargs.update({'response': response, 'backend': self})
+        return self.strategy.authenticate(*args, **kwargs)
+
+
+
+    def _get_uid(self):
+        return self.data['uid']
 
 
     def auth_params(self, state=None):
         client_id, client_secret = self.get_key_and_secret()
-        
+
         uri = self.get_redirect_uri(state)
         if self.REDIRECT_IS_HTTPS:
             uri = uri.replace('http://', 'https://')
-        
+
         params = {
-            'client_id': client_id,
-            'client_secret': client_secret,
-            'redirect_uri': uri
+            'urlredirect': uri,
+            'clientId': client_id,
+            'secretkey': client_secret
         }
-        if self.STATE_PARAMETER and state:
-            params['state'] = state
-        if self.RESPONSE_TYPE:
-            params['response_type'] = self.RESPONSE_TYPE
         return params
 
+    def get_user_id(self, details, response):
+        return details.get('email')
 
-    def auth_complete_params(self, state=None):
-        uri = self.get_redirect_uri(state)
-        if self.REDIRECT_IS_HTTPS:
-            uri = uri.replace('http://', 'https://')
-        return {
-            'uid': ''  # request auth code
-        }
 
     def get_user_details(self, response):
-    	log.error("this is response {}".format(response))
-        data = jwt.decode(response.get('id_token'), verify=False)
-        return {'username': data.get('firstname'),
-                'email': data.get('emailaddress'),
-                'fullname': data.get('firstname'),
-                'first_name': data.get('firstname')}
+        response = self._fill_fields(response)
+        return {'username': response.get('username'),
+                'email': response.get('emailaddress'),
+                'fullname': response.get('firstname'),
+                'first_name': response.get('firstname')}
+
+
+    def _fill_fields(self, data):
+        if data.get('firstname') is None:
+            data['firstname'] = data.get('email').split('@')[0]
+        if data.get('username') is None:
+            data['username'] = data.get('email').split('@')[0]
+        
+        return data
 
 
     def user_data(self, access_token, *args, **kwargs):
         """Loads user data from service. Implement in subclass"""
-        data = jwt.decode(access_token, verify=False)
+        data = self._fill_fields(kwargs.get('response'))
         return {'username': data.get('firstname'),
                 'email': data.get('emailaddress'),
                 'fullname': data.get('firstname'),
                 'first_name': data.get('firstname')}
 
+    def _get_creds(self):
+        client_id, client_secret = self.get_key_and_secret()
+        return {
+            'secretkey': client_secret,
+            'clientId': client_id
+            }
+
 
     def auth_headers(self):
-        return {'Content-Type': 'application/x-www-form-urlencoded',
-                'Accept': 'application/json'}
+        return {'Accept': 'application/json'}
 
 
     def pipeline(self, pipeline, pipeline_index=0, *args, **kwargs):
@@ -100,31 +128,17 @@ class TaleneticOAuth2(BaseOAuth2):
         add the incomming guid as a username field to the meta field on the user profile
         """
         out = self.run_pipeline(pipeline, pipeline_index, *args, **kwargs)
-        
         if not isinstance(out, dict):
             return out
 
         user = out.get('user')
-
-        user_profile = user.profile
-        new_meta = {'username': user.email.split('@')[0][0:-6]}
-
-        if len(user_profile.meta) > 0:
-            previous_meta = json.loads(user_profile.meta)
-            mixed_dicts =\
-                (previous_meta.items() + new_meta.items())
-            new_meta =\
-                {key: value for (key, value) in mixed_dicts}
-
-        user_profile.meta = json.dumps(new_meta)
-        user_profile.save()
 
         if user:
             user.social_user = out.get('social')
             user.is_new = out.get('is_new')
 
         return user
-        
+
 
     def revoke_token_params(self, token, uid):
         social_user = social_django.models.DjangoStorage.user.get_social_auth(provider=self.name, uid=uid)
@@ -132,3 +146,14 @@ class TaleneticOAuth2(BaseOAuth2):
             'id_token_hint': social_user.extra_data['jwt_token'],
             'state': self.get_session_state()
         }
+
+
+    def auth_url(self):
+        """Return redirect url"""
+        params = self.auth_params()
+        params = urlencode(params)
+        if not self.REDIRECT_STATE:
+            # redirect_uri matching is strictly enforced, so match the
+            # providers value exactly.
+            params = unquote(params)
+        return '{0}?{1}'.format(self.authorization_url(), params)
