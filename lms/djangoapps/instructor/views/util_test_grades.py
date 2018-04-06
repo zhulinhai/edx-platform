33058a9acb6e368acdace220b5b8538ef468eddf
Diego <rojastorrado@gmail.com>
2018-04-06 17:12:15 -0500

    Building report by section

--- lms/djangoapps/instructor/views/util_test_grades.py
+++ lms/djangoapps/instructor/views/util_test_grades.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
+import json
+
 from django.conf import settings
 from django.contrib.auth.models import User
 
 from student.models import CourseEnrollment
 from lms.djangoapps.grades.new.course_grade_factory import CourseGradeFactory
 from courseware import courses
-from opaque_keys.edx.keys import CourseKey
+
+from opaque_keys.edx.keys import CourseKey, UsageKey
+
+
+class Dummy(object):
+
+    def __init__(self):
+        self.is_staff = True
+
+    def user(self):
+        return User.objects.get(username="staff").is_staff
+
+    def is_staff(self):
+        return True
+
+    def has_user(self):
+        return True
 
 
 class ServiceGrades(object):
@@ -20,12 +38,37 @@ class ServiceGrades(object):
         for student in self.students:
             course_grade = CourseGradeFactory().create(student, self.course)
             gradeset = course_grade.summary
+            sections = course_grade.chapter_grades
             gradeset["username"] = student.username
             gradeset["fullname"] = "{} {}".format(student.first_name, student.last_name)
             course_grades.append(gradeset)
-        return course_grades
+
+        # We return a dict to access to these objects without iterate.
+        response = {
+            'course_grades': course_grades,
+            'sections': sections
+        }
+        return response
 
     def by_section(self):
+        gradeset = self.get_grades()
+        grades = gradeset['course_grades']
+        sections = gradeset['sections']        
+        by_section = {}
+    
+        for grade in grades:
+            by_section[grade["username"]] = {
+                "fullname": grade["fullname"]
+            }
+            for section in grade["section_breakdown"]:
+                if section.has_key('prominent'):
+                    if section['prominent'] == True:
+                        for chapter in sections.items():
+                            section_name = chapter[1]['display_name']
+                            by_section[grade["username"]].update({section_name: section['percent']})
+        return by_section
+
+    def by_assignment_type(self):
         gradeset = self.get_grades()
         by_section = {}
         for grade in gradeset:
