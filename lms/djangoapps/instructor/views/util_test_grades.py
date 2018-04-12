613e4e2abce848dd845b43780c39ca69f41c523d
Diego <rojastorrado@gmail.com>
2018-04-12 18:05:14 -0500

    Advance in CSV by assignment type report

--- lms/djangoapps/instructor/views/util_test_grades.py
+++ lms/djangoapps/instructor/views/util_test_grades.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import csv
+import json
 from datetime import datetime
 
 from django.conf import settings
@@ -8,9 +9,10 @@ from pytz import UTC
 
 from student.models import CourseEnrollment
 from lms.djangoapps.grades.new.course_grade_factory import CourseGradeFactory
-from lms.djangoapps.gating.api import get_entrance_exam_usage_key
 from courseware import courses
 from opaque_keys.edx.keys import CourseKey, UsageKey
+
+from openedx.core.djangoapps.content.course_structures.models import CourseStructure
 
 
 class ServiceGrades(object):
@@ -43,6 +45,7 @@ class ServiceGrades(object):
                 section_dict["username"] = course_grade_factory.user.username
                 score = course_grade_factory.score_for_chapter(section[0])
                 # Score object is a tuple: (earne, possible). We need only get earned value.
+                # import ipdb; ipdb.set_trace()
                 section_dict[section[1]["display_name"]] = score[0]
                 initial_header_rows.append(section[1]["display_name"])
 
@@ -57,18 +60,51 @@ class ServiceGrades(object):
 
     def by_assignment_type(self):
         course_grade = self.get_grades()
+        structure_course = CourseStructure.objects.get(course_id=self.course_key).structure_json
+        structure_course = json.loads(structure_course)
+
+        headers = []
         assignment_type_grades = []
+
+        # We use by_section method to get grades and chapters names and add it to the headers
+        # but first we need to proccess this result deleting the username key, due values of usernames
+        # will be the rows and not the headers.
+        chapters = self.by_section()
+        for chapter in chapters:
+            del chapter['username']
+        
+        # Build the initial rows for CSV
+        chapter_results = {}
+        for chapter_name, chapter_grade in chapter.items():
+            chapter_results[chapter_name] = chapter_grade
+            assignment_type_grades.append(chapter_results)
+            headers.append(chapter_name)
+
+
         for student in course_grade:
+            course_grade_factory = CourseGradeFactory().create(student["username"], self.course)
+            sections = course_grade_factory.chapter_grades
+
             results = {}
             results['username'] = student['username'].username
             scores_list = []
             for assignment_type in student["section_breakdown"]:
                 if assignment_type.has_key('prominent') and assignment_type['prominent'] == True:
-                    scores_list.append({assignment_type['category']: assignment_type['percent']})
-                    results['assignment_type'] = scores_list
+                    # scores_list.append({assignment_type['category']: assignment_type['percent']})
+                    # results['assignment_type'] = scores_list
+                    results[assignment_type['category']] = assignment_type['percent']
 
             assignment_type_grades.append(results)
 
+        for section in sections:
+            chapter_key = section.to_deprecated_string()
+            section_block = structure_course['blocks'][chapter_key]
+            display_name = section_block['display_name']
+            subsections = section_block['children']
+            for sequential_key in subsections:
+                assignment_type = structure_course['blocks'][sequential_key]['format']
+                headers.append("{} - {}".format(display_name, assignment_type))
+        
         return assignment_type_grades
 
 
