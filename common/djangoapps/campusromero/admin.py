c29c2a318b78fed36e388a639a99aee5cf4d7236
Eric Herrera <eric.herrera@edunext.co>
2018-05-09 13:48:38 -0500

    Fix unregistered class, User instead of UserAdmin.

--- common/djangoapps/campusromero/admin.py
+++ common/djangoapps/campusromero/admin.py
@@ -29,12 +29,11 @@ from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 from django.contrib.admin import SimpleListFilter
 from datetime import date
 
-from django.contrib.auth.admin import UserAdmin
 
 csrf_protect_m = method_decorator(csrf_protect)
 sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
 
-admin.site.unregister(UserAdmin)
+admin.site.unregister(User)
 
 class UserResource(resources.ModelResource):
     #columnas de UserProfile
