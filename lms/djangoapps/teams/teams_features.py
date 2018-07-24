ae3ff271cfaae5beeabb61ee7caaad58fcd2e395
andrey <andrey.canon@edunext.co>
2018-07-24 14:29:47 -0500

    - Remove unnecessary files - Remove previous methods - Changing names of functions and variables - Changing setting's names

--- lms/djangoapps/teams/teams_features.py
+++ lms/djangoapps/teams/teams_features.py
@@ -1,124 +1,41 @@
-import logging
-
-from django.conf import settings
-from web_fragments.fragment import Fragment
-
+"""Methods for teams modifications"""
 from courseware.courses import get_course_by_id
-from courseware.model_data import FieldDataCache
-from courseware.module_render import get_module_for_descriptor
-from openedx.core.djangoapps.crawlers.models import CrawlersConfig
 from student.models import CourseEnrollmentManager
 from opaque_keys.edx.locator import BlockUsageLocator
-
-from xmodule.x_module import STUDENT_VIEW
+from opaque_keys import InvalidKeyError
 
 CONTENT_DEPTH = 2
-SECTION_NAME = getattr(settings, "TEAM_SECTION", "Teams")
-LOG = logging.getLogger(__name__)
 
-
-class ModifyTeams(object):
-    """this class alows to modifies the team's behavior"""
-
-    def __init__(self, request, user, course_key):
-
-        self.effective_user = user
+class CourseTeamsFeatures(object):
+    """A class that provides the necessary settings and info for teams"""
+    def __init__(self, course_key):
+        course = get_course_by_id(course_key, CONTENT_DEPTH)
         self.course_key = course_key
-        self.course = get_course_by_id(self.course_key, CONTENT_DEPTH)
-        self._prefetch_and_bind_course(request)
+        self.teams_configuration = course.teams_configuration
 
-        self.section = self._find_section()
-        self.subsection = self._find_subsection()
-
-    def get_fragment(self):
-        """This method allows to get a fragment from a course's section"""
-        unit = self._find_unit()
-        if unit:
-            frag = unit.render(STUDENT_VIEW)
-            return frag
-        return None
-
-    def _find_section(self):
+    def get_replacement_locator(self):
         """
-        Finds the section where the rocketChat's unit was defined.
+        This method returns the locator for a unit, to do that this gets the unit location_id
+        from teams_configuration, to set that go to studion->advanced settings-> teams_configuration
         """
-        sections = self.course.get_children()
-        for section in sections:
-            if section.display_name==SECTION_NAME:
-                return section
-        LOG.warning("The section, where the rocketChat's unit was defined, is missing")
-        return None
-
-    def _find_subsection(self):
-        """
-        Finds the subsection.
-        """
-        if self.section:
-            subsections = self.section.get_children()
-            if len(subsections)==1:
-                return subsections[0]
-            else:
-                LOG.warning("Only one subsection must be defined")
-        return None
-
-    def _find_unit(self):
-        """
-        Finds the rocketchat's unit.
-        """
-        if self.subsection:
-            units = self.subsection.get_children()
-            if len(units)==1:
-                return units[0]
-            else:
-                LOG.warning("Only one unit must be defined")
-        return None
-
-    def _prefetch_and_bind_course(self, request):
-        """
-        Prefetches all descendant data for the requested section and
-        sets up the runtime, which binds the request user to the section.
-        """
-        self.field_data_cache = FieldDataCache.cache_for_descriptor_descendents(
-            self.course_key,
-            self.effective_user,
-            self.course,
-            depth=CONTENT_DEPTH,
-            read_only=CrawlersConfig.is_crawler(request),
-        )
-        self.course = get_module_for_descriptor(
-            self.effective_user,
-            request,
-            self.course,
-            self.field_data_cache,
-            self.course_key,
-            course=self.course,
-        )
-
-    def discussion_is_available(self):
-        """
-        This method checks if teams are using discussion
-        """
-        if "discussion_is_available" in self.course.teams_configuration:
-            return self.course.teams_configuration["discussion_is_available"]
-        return True
-
-    def get_rocket_chat_locator(self):
-        """
-        This method gets the locator for a unique component in a unit.
-        """
-        if "rocketchat-xblock-id" in self.course.teams_configuration:
-            block_id = self.course.teams_configuration["rocketchat-xblock-id"]
+        try:
+            block_id = self.teams_configuration["replacement_location_id"]
             block_type = "vertical"
             locator = BlockUsageLocator(self.course_key, block_type, block_id)
-        else:
-            unit = self._find_unit()
-            child = unit.get_children()
-            child = child[0]
-            locator = child.parent
-        return locator.to_deprecated_string()
+            return locator
+        except (KeyError, InvalidKeyError):
+            return None
 
-def get_users_enrolled(course_key):
-    """Return a generator with the username of every user in the course"""
-    users = CourseEnrollmentManager().users_enrolled_in(course_key)
-    for user in users:
-        yield user.username
+    def get_users_enrolled(self):
+        """Return a generator with the username of every user in the course"""
+        users = CourseEnrollmentManager().users_enrolled_in(self.course_key)
+        for user in users:
+            yield user.username
+
+    def is_teams_locked(self):
+        """Returns if teams is locked, this setting can be configured on studio"""
+        return self.teams_configuration.get("teams_locked")
+
+    def is_discussion_disable(self):
+        """Returns if discussion is disabled, this setting can be configured on studio"""
+        return self.teams_configuration.get("disable_default_discussion")
