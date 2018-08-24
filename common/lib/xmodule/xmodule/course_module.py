621e81c97607893b18b7fb21f5cf71d71d7eb2cb
andrey <andrey.canon@edunext.co>
2018-08-24 12:39:20 -0500

    Add new fields on studio in order to get custom block types and required ids

--- common/lib/xmodule/xmodule/course_module.py
+++ common/lib/xmodule/xmodule/course_module.py
@@ -920,6 +920,20 @@ class CourseFields(object):
         scope=Scope.settings, default=None
      )
 
+    custom_block_type_keys = List(
+        display_name=_("Custom Block Types"),
+        help=_("Specify what kind of blocks will be mark as completed."),
+        default=[],
+        scope=Scope.settings
+    )
+
+    required_activity_ids = List(
+        display_name=_("Required Activity Ids"),
+        help=_("Specify what Activities will be required for the course"),
+        default=[],
+        scope=Scope.settings
+    )
+
 
 class CourseModule(CourseFields, SequenceModule):  # pylint: disable=abstract-method
     """
