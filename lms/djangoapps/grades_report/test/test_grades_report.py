664490f1956fb08704a741efbd33a9998aa5721b
Squirrel18 <daniel.quiroga@edunext.co>
2018-09-19 18:25:55 -0500

    Add more test to reports methods.

--- lms/djangoapps/grades_report/test/test_grades_report.py
+++ lms/djangoapps/grades_report/test/test_grades_report.py
@@ -4,6 +4,7 @@ Test for grade services
 from lms.djangoapps.grades_report.grade_services import (
     GradeServices,
     BySectionGradeServices,
+    ByAssignmentTypeGradeServices,
     EnhancedProblemGradeServices,
 )
 from lms.djangoapps.grades_report.test.test_course import CourseTest
@@ -18,6 +19,7 @@ class GradeServicesTest(CourseTest):
         super(GradeServicesTest, cls).setUpClass()
         cls.grade_services = GradeServices(cls.course.id.html_id())
         cls.by_section_grades_services = BySectionGradeServices(cls.course.id.html_id())
+        cls.by_assignment_type_services = ByAssignmentTypeGradeServices(cls.course.id.html_id())
         cls.enhanced_problem_grades_services = EnhancedProblemGradeServices(cls.course.id.html_id())
 
 
@@ -40,7 +42,7 @@ class GradeServicesTest(CourseTest):
         self.assertIn('up_to_date_grade', by_section_data[0])
 
 
-    def test_enhanced_problem_grade(self):
+    def test_enhanced_problem_grade_key(self):
         problem_grade_report = self.enhanced_problem_grades_services.enhanced_problem_grade()
         self.assertIn('problem_breakdown', problem_grade_report[0])
 
@@ -106,3 +108,53 @@ class GradeServicesTest(CourseTest):
         self.assertEquals(len(section_breakdown[1]['assignment_types']), 2)
         # In the third section must be exist three assignment types: Homework, Exams and Quiz.
         self.assertEquals(len(section_breakdown[2]['assignment_types']), 3)
+
+
+    def test_by_section_report(self):
+        grades_by_section = self.by_section_grades_services.by_section()
+        self.assertNotIn('section_at_breakdown', grades_by_section[0])
+        self.assertIn('grades', grades_by_section[0])
+        self.assertIn('up_to_date_grade', grades_by_section[0])
+        # 3, is the total of sections in the course, included unreleased section.
+        self.assertEquals(len(grades_by_section[0]['grades']), 3)
+        up_to_date_grade_percent = grades_by_section[0]['up_to_date_grade']['percent']
+        up_to_date_grade_section = grades_by_section[0]['up_to_date_grade']['calculated_until_section']
+        # All course sections must be calculated, cause no block_id is passed to the method.
+        self.assertEquals(up_to_date_grade_section, 'All course sections')
+        self.assertAlmostEqual(up_to_date_grade_percent, grades_by_section[0]['percent'])
+        grades = grades_by_section[0]['grades']
+        total_percent_in_grades = sum(x['percent'] for x in grades)
+        self.assertAlmostEqual(total_percent_in_grades, grades_by_section[0]['percent'])
+
+
+    def test_by_assignment_type_report(self):
+        grades_by_assignment_type = self.by_assignment_type_services.by_assignment_type()
+        self.assertNotIn('section_at_breakdown', grades_by_assignment_type[0])
+        self.assertIn('grades', grades_by_assignment_type[0])
+        # The sum of max_possible_grade at every assignment type,
+        # must be equal to max_possible_percent in that section.
+        first_course_section = grades_by_assignment_type[0]['grades'][0]
+        first_max_possible_grade = sum(x['max_possible_grade'] for x in first_course_section['assignment_types'])
+        self.assertAlmostEqual(first_max_possible_grade, first_course_section['max_possible_percent'])
+        second_course_section = grades_by_assignment_type[0]['grades'][1]
+        second_max_possible_grade = sum(x['max_possible_grade'] for x in second_course_section['assignment_types'])
+        self.assertAlmostEqual(second_max_possible_grade, second_course_section['max_possible_percent'])
+        third_course_section = grades_by_assignment_type[0]['grades'][2]
+        third_max_possible_grade = sum(x['max_possible_grade'] for x in third_course_section['assignment_types'])
+        self.assertAlmostEqual(third_max_possible_grade, third_course_section['max_possible_percent'])
+        # The sum of grades at every assignment type,
+        # must be equal to the percent in that section.
+        first_total_grade = sum(x['grade'] for x in first_course_section['assignment_types'])
+        self.assertAlmostEqual(first_total_grade, first_course_section['percent'])
+        second_total_grade = sum(x['grade'] for x in second_course_section['assignment_types'])
+        self.assertAlmostEqual(second_total_grade, second_course_section['percent'])
+        third_total_grade = sum(x['grade'] for x in third_course_section['assignment_types'])
+        self.assertAlmostEqual(third_total_grade, third_course_section['percent'])
+
+
+    def test_enhanced_problem_grade_report(self):
+        enhanced_problem_grades = self.enhanced_problem_grades_services.enhanced_problem_grade()
+        self.assertIn('problem_breakdown', enhanced_problem_grades[0])
+        problem_breakdown = enhanced_problem_grades[0]['problem_breakdown']
+        # 7, is the total of problems attempted in the course.
+        self.assertEquals(len(problem_breakdown), 7)
