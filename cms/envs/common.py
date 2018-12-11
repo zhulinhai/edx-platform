ff1e116fe07f617e36398514a507dd03f93283c2
Eric Herrera <eric.herrera@edunext.co>
2018-12-11 15:19:09 -0500

    Increase CMS Files and Uploads allowed size to 20 MB.

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1301,10 +1301,10 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = None
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
 ### Size of chunks into which asset uploads will be divided
-UPLOAD_CHUNK_SIZE_IN_MB = 10
+UPLOAD_CHUNK_SIZE_IN_MB = 20
 
 ### Max size of asset uploads to GridFS
-MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB = 10
+MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB = 20
 
 # FAQ url to direct users to if they upload
 # a file that exceeds the above size
